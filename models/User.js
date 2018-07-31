1b21ef82bb96ec3c9643bd8ae635a315143b6cb1
MayuriR29 <mayuri.gr@gmail.com>
2018-07-31 17:11:11 +0800

    Added new validate password method for user model

--- models/User.js
+++ models/User.js
@@ -4,37 +4,40 @@ const uniqueValidator = require("mongoose-unique-validator");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const userSchema = mongoose.Schema(
-	{
-		username: {
-			type: String,
-			required: [true, "username is required!"],
-			match: [ /^[a-zA-Z0-9]{6,20}\b/, "username format is invalid" ],
-			lowercase: true,
-			unique: true,
-			index: true
-		},
-		hash: String,
-        salt: String,
-        email: { 
-            type: String,
-            required: [true, "email is required!"],
-        }
-	},
-	{ timestamps: true }
+  {
+    username: {
+      type: String,
+      required: [true, "username is required!"],
+      match: [/^[a-zA-Z0-9]{6,20}\b/, "username format is invalid"],
+      lowercase: true,
+      unique: true,
+      index: true
+    },
+    hash: String,
+    salt: String,
+    email: {
+      type: String,
+      required: [true, "email is required!"]
+    }
+  },
+  { timestamps: true }
 );
 
 userSchema.plugin(uniqueValidator, { message: "username should be unique" });
 
 // use ES5 function to prevent `this` from becoming undefined
 userSchema.methods.setHashedPassword = function(password) {
-	this.salt = generateSalt();
-	this.hash = hashPassword(password, this.salt);
+  this.salt = generateSalt();
+  this.hash = hashPassword(password, this.salt);
 };
 
-// use ES5 function to prevent `this` from becoming undefined
 userSchema.methods.validatePassword = function(password) {
-	if (!password) return false;
-	return this.hash === hashPassword(password, this.salt);
+  return !(password.length < 8 || password.length > 20);
+};
+// use ES5 function to prevent `this` from becoming undefined
+userSchema.methods.verifyPassword = function(password) {
+  if (!password) return false;
+  return this.hash === hashPassword(password, this.salt);
 };
 
 // userSchema.methods.toDisplay = function() {
@@ -51,14 +54,14 @@ userSchema.methods.validatePassword = function(password) {
 // };
 
 const generateSalt = () => {
-	return crypto.randomBytes(16).toString("hex");
+  return crypto.randomBytes(16).toString("hex");
 };
 
 const hashPassword = (password, salt) => {
-	const hash = crypto
-		.pbkdf2Sync(password, salt, 10000, 512, "sha512")
-		.toString("hex");
-	return hash;
+  const hash = crypto
+    .pbkdf2Sync(password, salt, 10000, 512, "sha512")
+    .toString("hex");
+  return hash;
 };
 
 const User = mongoose.model("User", userSchema);
