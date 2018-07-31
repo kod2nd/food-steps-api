65370e40c0d7cc74e8124080f46908ed8d862111
kod2nd <colinongengkiat@gmail.com>
2018-07-31 10:46:30 +0800

    added index route simple Welcome message with tests

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const indexRouter = express.Router()
 indexRouter.use(express.json())
 
 
-indexRouter.get('/', res.json({}) )
+indexRouter.get('/', (req, res, next) => { res.json({ message: "Welcome!" }) })
 
 module.exports = (app) => {
     app.use('/', indexRouter)
