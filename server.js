0b074a8ea9335bd924aa219cecbf2d3d742a380f
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-31 11:20:54 +0800

    dummy commit to test that pipeline works

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ db.on('error', error => {
     console.error('An error occurred!', error)
 })
 
+
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${server.address().port}...`);
 });
