16a665341dab06dccc43bdeaebae204a52d3433a
MayuriR29 <mayuri.gr@gmail.com>
2018-07-31 15:00:50 +0800

    added morgan

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-app.use(express.json())
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+require("dotenv").config();
+
+app.use(logger("dev"));
+app.use(express.json());
 
 // Routes
 const indexRouter = require('./routes/index')
 const accountRouter = require('./routes/accountRouter')
 const userLocationRouter = require('./routes/userLocationRouter')
 
-indexRouter(app)
+indexRouter(app);
 accountRouter(app);
 userLocationRouter(app);
 
 // Error Handlers
 app.use((req, res, next) => {
-    res.status(404).json({error: "Invalid request!"});
-})
+  res.status(404).json({ error: "Invalid request!" });
+});
 
-module.exports = app
+module.exports = app;
