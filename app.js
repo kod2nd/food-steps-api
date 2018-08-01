18e149d4e65124ba5e8cfbc4f9c4b486c4730078
MayuriR29 <mayuri.gr@gmail.com>
2018-08-01 14:30:10 +0800

    install and used cookie parser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 require("dotenv").config();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cookieParser());
+
 
 // Routes
 const indexRouter = require('./routes/index')
