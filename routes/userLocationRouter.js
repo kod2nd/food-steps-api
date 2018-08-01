7ff5fff3b2a9b5e5f0f93fd7318237cc03a4de17
MayuriR29 <mayuri.gr@gmail.com>
2018-08-01 17:46:28 +0800

    Format code for userLocationRouter

--- routes/userLocationRouter.js
+++ routes/userLocationRouter.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const userLocationsRouter = express.Router()
+const express = require("express");
+const userLocationsRouter = express.Router();
 
-
-const userLocationService = require('../middlewares/userLocationService')
+const userLocationService = require("../services/userLocationService");
 const handleAsyncError = require("../utils/handleAsyncError");
 
-userLocationsRouter.use(express.json())
+userLocationsRouter.use(express.json());
 
 // Get needs to show a list of user locations - still yet to be done
-userLocationsRouter.get('/:id', handleAsyncError(userLocationService.sayHello))
+userLocationsRouter.get("/:id", handleAsyncError(userLocationService.sayHello));
 
-userLocationsRouter.post('/:id', handleAsyncError(userLocationService.createUserLocation))
+userLocationsRouter.post(
+  "/:id",
+  handleAsyncError(userLocationService.createUserLocation)
+);
 
-module.exports = (app) => {
-    app.use('/locations/user', userLocationsRouter)
-}
+module.exports = app => {
+  app.use("/locations/user", userLocationsRouter);
+};
