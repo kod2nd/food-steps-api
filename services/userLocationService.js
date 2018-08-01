62eb63bffbf192fb54a5b097d06894af249cfda6
kod2nd <colinongengkiat@gmail.com>
2018-08-01 19:24:15 +0800

    refactored and  moved helper functions from userLocationsService to SerivceHelper

--- services/userLocationService.js
+++ services/userLocationService.js
@@ -1,44 +1,14 @@
 const GlobalLocation = require("../models/GlobalLocation");
 const UserLocation = require("../models/UserLocation");
+const {getExistingUserLocations, isUserLocationExists, getGlobalLocationIdIfExists, createGlobalLocation} = require("./serviceHelper/userLocationHelper")
 
-// Helper functions
-const getExistingUserLocations = async (userId) => {
-  const userExistingLocations = await UserLocation.find({ userId }).populate('globalLocation');
-  return userExistingLocations
-}
 
-const isUserLocationExists = async (userId, lat, lng) => {
-  const userExistingLocations = await getExistingUserLocations(userId)
-
-  const index = userExistingLocations.findIndex(location => {
-    return Number(location.globalLocation.lat) === lat && Number(location.globalLocation.lng) === lng
-  })
-  return index !== -1
-}
-
-const getGlobalLocationIdIfExists = async (lat, lng) => {
-  const location = await GlobalLocation.findOne({ lat, lng });
-  return location ? location._id : undefined;
-};
-
-const createGlobalLocation = async (lat, lng, geocodedLocationName) => {
-  const globalLocation = new GlobalLocation({
-    lat,
-    lng,
-    geocodedLocationName
-  });
-  const savedGlobalLocation = await globalLocation.save();
-  return savedGlobalLocation._id;
-};
-
-// Services
 const displayAllUserLocations = async (req, res, next) => {
   const userId = req.params.id
   const existingUserLocations = await getExistingUserLocations(userId)
 
   res.json(existingUserLocations);
 };
-
 
 const createUserLocation = async (req, res, next) => {
   const userId = req.params.id
