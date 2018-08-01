f240311f3f9023eea25373a11e9f27fabd4a1d4f
kod2nd <colinongengkiat@gmail.com>
2018-08-01 17:24:05 +0800

    Added async error handler for userLocations router

--- routes/userLocationRouter.js
+++ routes/userLocationRouter.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const userLocationsRouter = express.Router()
 
+
 const userLocationService = require('../middlewares/userLocationService')
+const handleAsyncError = require("../utils/handleAsyncError");
 
 userLocationsRouter.use(express.json())
 
 // Get needs to show a list of user locations - still yet to be done
-userLocationsRouter.get('/:id', userLocationService.sayHello)
+userLocationsRouter.get('/:id', handleAsyncError(userLocationService.sayHello))
 
-userLocationsRouter.post('/:id', userLocationService.createUserLocation )
+userLocationsRouter.post('/:id', handleAsyncError(userLocationService.createUserLocation))
 
 module.exports = (app) => {
     app.use('/locations/user', userLocationsRouter)
