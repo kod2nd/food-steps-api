9c847e5db81b493a0092bec278c1f2b62f98b26f
ngyl88 <ng_yl88@hotmail.com>
2018-08-03 11:36:30 +0800

    added error handler for uncaught CastError

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,16 +1,19 @@
+const { CastError } = require("mongoose").Error;
 const {
   VALIDATION_ERROR,
   UNAUTHORISED_ERROR
 } = require("../utils/getCustomErrors");
 
 const errorHandler = (err, req, res, next) => {
-  if (err.name === VALIDATION_ERROR) {
+  if (err instanceof CastError || err.name === VALIDATION_ERROR) {
     // will enter here for CastError and ValidatorError (custom, required and unique validators)
     // that occur during operations that involves writing to db
+    // 2. will enter here for uncaught mongoose CastError
     return res.status(400).json(err.message);
   }
 
   if (err.name === UNAUTHORISED_ERROR) return res.status(401).json(err.message);
+  next(err);
 };
 
 module.exports = errorHandler;
