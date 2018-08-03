f5de18845b3a891594b86dd915d013a2755ee6d2
ngyl88 <ng_yl88@hotmail.com>
2018-08-03 10:16:59 +0800

    added timestamp to global location model

--- models/GlobalLocation.js
+++ models/GlobalLocation.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const schema = Schema({
-    lat: {type: Number, required: true},
-    lng: {type: Number, required: true},
-    geocodedLocationName: {type: String, required: true},
+const schema = Schema(
+  {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    geocodedLocationName: { type: String, required: true },
     reverseGeocodeAddress: String,
     collectiveRating: Number,
     numOfUserRating: Number
+  },
+  { timestamps: true }
+);
 
-});
-
-module.exports = mongoose.model('GlobalLocation', schema);
+module.exports = mongoose.model("GlobalLocation", schema);
